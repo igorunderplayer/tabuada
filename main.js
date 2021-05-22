9cd6d939a3267bb47838d9b0f1c731d5c9662f4b
igorunderplayer <igorschug6@gmail.com>
2021-05-22 18:44:29 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function change () {
+    var x = document.querySelector('.box input').value || 0
+    var tabuada = [];
+    for (var i = 0;i < 11;i++) {
+        let val = x * i;
+        tabuada += (`<p> ${x} * ${i} = ${val} </p>`)
+    }
+    document.querySelector('.box .tabuada').innerHTML = tabuada
+}
