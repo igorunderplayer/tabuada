473bc1cb70377f44404677c7970c070af99dc87c
igorunderplayer <igorschug6@gmail.com>
2021-05-23 15:45:14 -0300

    a

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function change () {
     var x = document.querySelector('.box input').value || 0
+    var size = Number(document.querySelector('.box select').value) || 10
+    console.log(size)
     var tabuada = "";
-    for (var i = 0;i < 11;i++) {
+    for (var i = 0;i < size + 1;i++) {
         let val = x * i;
-        tabuada += (`<p> ${x} * ${i} = ${val} </p>`)
+        tabuada += (`<p> ${x} x ${i} = ${val} </p><hr>`)
     }
     document.querySelector('.box .tabuada').innerHTML = tabuada
 }
